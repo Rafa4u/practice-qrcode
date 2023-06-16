a14ada7ac93d23e880d1ef7b65a5ad9a92e28854
rafa s <fontana.rafael@gmail.com>
2023-06-16 19:22:59 -0300

    almost working

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container'),
+qrInput = container.querySelector('.form input'),
+generationBtn = container.querySelector('.form button'),
+qrImg = container.querySelector('.qr-code .img');
+
+generationBtn.addEventListener('click', () => {
+    let qrValue = qrInput.value;
+    if(!qrValue){
+        alert('Preencha o campo com uma URL ou texto')
+        return;
+    }
+    generationBtn.innerText = "Gerando um Qr Code...";
+    qrImg.scr = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data${qrValue}`;
+    qrImg.addEventListener('load', () => {
+        generationBtn.innerText = "Gerar QrCode";
+        container.classList.add('active');
+    });
+});
+
+qrInput.addEventListener('keyup', () => {
+    if (!qrInput.value){
+        container.classList.remove('active');
+    };
+});
